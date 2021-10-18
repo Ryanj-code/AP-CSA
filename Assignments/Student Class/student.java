4b4efec5287b413c41e1ae410c8a7edc133e515c
Ryan Jiang <83621881+Ryanj-code@users.noreply.github.com>
2021-10-18 12:02:47 -0400

    Create student.java

---
+++ Assignments/Student Class/student.java
@@ -0,0 +1,35 @@
+public class Student{
+  
+  private String name;
+  private int gradeLvl;
+  private int[] grades;
+  private double average;
+
+  public Student(String name, int gradeLvl, int[] grades){
+    this.name = name;
+    this.gradeLvl = gradeLvl;
+    this.grades = grades;
+  }
+  
+  public String status(){
+    return "Student name : " + name + "\nGrade Level : " + gradeLvl ;
+  }
+
+  public double getAverage(){
+    double average = 0;
+    for (int i = 0; i < grades.length;i ++){
+      average += grades[i];
+    }
+    this.average = average;
+    return average/grades.length;
+  }
+
+  public String passOrFail() {
+    if (this.average > 65){
+      return "Pass";
+    }
+    else{
+      return "Fail";
+    }    
+  }
+}
