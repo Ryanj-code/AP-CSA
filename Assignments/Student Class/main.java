1738ba78e3c09767a664813143c44fd11b53a482
Ryan Jiang <jiangr0421@gmail.com>
2021-10-21 10:55:33 -0400

    Update main.java

--- Assignments/Student Class/main.java
+++ Assignments/Student Class/main.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Main {
   public static void main(String[] args) {
     int[] grades = {90, 80, 85, 88, 75, 94};
-    Student person = new Student("Jeff", 10, grades);
+    student person = new student("Jeff", 10, grades);
 
     System.out.println(person.status());
     System.out.println(person.getAverage());
