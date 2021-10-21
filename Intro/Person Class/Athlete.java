8c90349e2844f072f321916efd37a63162091ca4
Ryan Jiang <jiangr0421@gmail.com>
2021-10-21 17:23:47 -0400

    Update and rename athlete.java to Athlete.java

--- Intro/Person Class/Athlete.java
+++ Intro/Person Class/Athlete.java
@@ -1,8 +1,8 @@
-public class athlete extends person {
+public class Athlete extends Person {
   private String sport;
   private String position;
   
-  public athlete(double height, double weight, int age, String name, String gender, String eyeColor, String sport, String position) {
+  public Athlete(double height, double weight, int age, String name, String gender, String eyeColor, String sport, String position) {
     super(height, weight, age, name, gender, eyeColor);
 
     this.sport = sport;
