8f9fe0649c6453621d52e081f3e2fee450da47de
Ryan Jiang <jiangr0421@gmail.com>
2021-10-21 09:53:21 -0400

    Create main.java

---
+++ Projects/Point Class/main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Main {
+
+  public static void main(String[] args) {
+    int x = 0;
+    int y = 1;
+    Point pt = new Point(x, y);
+    
+    System.out.println("The point is " + pt.toString());
+    System.out.println(pt.getSlope(4,5));
+    System.out.println(pt.getDistance(4,5));
+    System.out.println(pt.getQuadrant());
+    System.out.println(pt.translate("right", 2));
+    System.out.println(pt.reflection("x"));
+    System.out.println(pt.isCollinear(2, 2, 3, 3));
+  }
+}
