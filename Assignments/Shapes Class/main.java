d218020d8b97d98ebcc9310c4766d5be3b24e6db
Ryan Jiang <jiangr0421@gmail.com>
2021-10-21 10:30:09 -0400

    Create main.java

---
+++ Assignments/Shapes Class/main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+    Triangles t = new Triangles(3, 3, 4, 5);
+    Circles c = new Circles(0, 5);
+    Quadrilateral q= new Quadrilateral(4, 4, 8);
+
+    System.out.println(t.getPerimeter());
+    System.out.println(t.getType());
+    System.out.println(t.toString());
+
+    System.out.println(c.getPerimeter());
+    System.out.println(c.getArea());
+    System.out.println(c.toString());
+
+    System.out.println(q.getArea());
+    System.out.println(q.getPerimeter());
+    System.out.println(q.toString());
+  }
+}
