fbb2bcc5206bf23f987cabf8d7d444aa0ccebf07
Ryan Jiang <jiangr0421@gmail.com>
2021-10-21 17:30:31 -0400

    Update and rename Projects/Student Class+/middle.java to Projects/Student Class Extension/Middle.java

--- Projects/Student Class Extension/Middle.java
+++ Projects/Student Class Extension/Middle.java
@@ -1,8 +1,8 @@
-public class middle extends student{
+public class Middle extends Student{
   private int avgYears;
   private int cost;
 
-  public middle(String name, String gender, int age, int avgYears, int cost){
+  public Middle(String name, String gender, int age, int avgYears, int cost){
     super(name,gender,age);
     this.avgYears = avgYears;
     this.cost = cost;
