c96b483a5f1eb9f69f00ae8118de9899e5678065
Ryan Jiang <jiangr0421@gmail.com>
2021-10-21 10:31:37 -0400

    Create shapes.java

---
+++ Assignments/Shapes Class/shapes.java
@@ -0,0 +1,11 @@
+public class Shapes{
+  private int sides;
+
+  public Shapes(int sides){
+    this.sides = sides;
+  }
+
+  public void returnSides(){
+    System.out.println("This shape has " + sides + " sides");
+  }
+}
