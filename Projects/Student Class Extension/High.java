8eb99d030afec31b175bb612ef228a93cdd1afad
Ryan Jiang <jiangr0421@gmail.com>
2021-10-21 17:31:32 -0400

    Update and rename Projects/Student Class+/high.java to Projects/Student Class Extension/High.java

--- Projects/Student Class Extension/High.java
+++ Projects/Student Class Extension/High.java
@@ -1,8 +1,8 @@
-public class high extends student{
+public class High extends Student{
   private int avgYears;
   private int cost;
 
-  public high(String name, String gender, int age, int avgYears, int cost, int scoreSAT){
+  public High(String name, String gender, int age, int avgYears, int cost, int scoreSAT){
     super(name,gender,age);
     this.avgYears = avgYears;
     tis.cost = cost;
