e354e26586c7c1cef56f856c4eb16c20924133e3
Ryan Jiang <jiangr0421@gmail.com>
2021-10-21 10:57:13 -0400

    Update student.java

--- Intro/Person Class/student.java
+++ Intro/Person Class/student.java
@@ -1,10 +1,10 @@
-public class Student extends Person {
+public class student extends person {
 
   private double gradeAverage;
   private String schedule;
   private int IDNumber;
 
-  public Student(double height, double weight, int age, String name, String gender, String eyeColor, double gradeAverage, String schedule, int IDNumber) {
+  public student(double height, double weight, int age, String name, String gender, String eyeColor, double gradeAverage, String schedule, int IDNumber) {
     super(height, weight, age, name, gender, eyeColor);
 
     this.gradeAverage = gradeAverage;
