8d00f26069ccf62290b7b5999ac114b3733885d8
Ryan Jiang <jiangr0421@gmail.com>
2021-10-21 10:32:08 -0400

    Create triangles.java

---
+++ Assignments/Shapes Class/triangles.java
@@ -0,0 +1,31 @@
+public class Triangles extends Shapes{
+  private int sideA;
+  private int sideB;
+  private int sideC;
+
+  public Triangles (int sides, int sideA, int sideB, int sideC){
+    super(sides);
+    this.sideA = sideA;
+    this.sideB = sideB;
+    this.sideC = sideC;
+  }
+
+  public String toString(){
+    return "Side A : " + sideA +"\nSide B : " + sideB + "\nSide C : " + sideC;
+  }
+
+  public int getPerimeter(){
+    return sideA + sideB + sideC;
+  }
+
+  public String getType(){
+    if(sideA == sideB && sideB == sideC){
+      return "Equilateral Triangle";
+    }
+    else if(sideA != sideB && sideA != sideC && sideB != sideC)
+      return "Scalene Triangle";
+    else{
+      return "Isosceles Triangle";
+    }
+  }
+}
