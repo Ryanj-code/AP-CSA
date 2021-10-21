e87ea1c0837595ad4ed49db7c6c8dac2f69e7bca
Ryan Jiang <jiangr0421@gmail.com>
2021-10-21 10:51:38 -0400

    Create student.java

---
+++ Projects/Student Class+/student.java
@@ -0,0 +1,19 @@
+public class student{
+  private String name;
+  private String gender;
+  private int age;
+
+  public student(String name, String gender, int age){
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+  }
+
+  public String toString(){
+    return "Name : " + name + "\nGender : " + gender + "\nAge : " + age;
+  }
+
+  public double getAvg(double a, double b, double c, double d, double e){
+    return (a + b + c + d + e)/5;  
+  }
+}
