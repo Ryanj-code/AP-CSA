f3184324880659c3bd7bcd044a2da4cccf72f248
Ryan Jiang <jiangr0421@gmail.com>
2021-10-21 10:47:35 -0400

    Create main.java

---
+++ Assignments/Student Class+/main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    Elementary Bryan = new Elementary("Bryan", "male", 7, 5, 0);
+    
+    Middle Alex = new Middle("Alex", "male", 12, 3, 0);
+    
+    High Evan = new High("Evan", "male", 16, 4, 0, 1300);
+
+    College Hans = new College("Han", "male", 20, "Computer Programming", 35000);
+  }
+}
